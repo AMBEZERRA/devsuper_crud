ecef0b22063dbf4ce4b113d4086f317fc24c2340
Allisson Bezerra <allisson.geografia@gmail.com>
2023-09-26 09:14:36 -0300

    Confingurando pom.xml e properties no resource

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Maria Brown', 'maria@gmail.com', '988888888', '123456', '2001-07-25');
+INSERT INTO tb_user (name, email, phone, password, birth_date) VALUES ('Alex Green', 'alex@gmail.com', '977777777', '123456', '1987-12-13');
